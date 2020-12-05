21c24ad0cab693e1883d2e327c6744c14ffd82b7
kamlesh Mitawa <kamlesh.k@squareyards.com>
2020-12-05 16:05:13 +0530

    completed

--- CountryInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CountryInput = (props) => {
-    console.log(props, 'peopsggggggggg')
-    return(
-        <input type="text" name="country" onChange={onChangeHandler} value={countryInput} />
-    )
-}
-export default CountryInput
